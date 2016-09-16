98f925371ada6a18195c35a75b573b26657bb380
Sharif Nassar <sharif@mrwacky.com>
2016-09-16 13:25:27 -0700

    Add a git ugly default if the lookup() fails

--- main.tf
+++ main.tf
@@ -9,6 +9,5 @@ variable "storagetype" {
 }
 
 output "ami_id" {
-    value = "${lookup(var.all_amis, format("%s-%s-%s-%s-%s", var.region, var.distribution, var.architecture, var.virttype, var.storagetype))}"
+    value = "${lookup(var.all_amis, format("%s-%s-%s-%s-%s", var.region, var.distribution, var.architecture, var.virttype, var.storagetype), "ERROR: no AMI matching '${format("%s-%s-%s-%s-%s", var.region, var.distribution, var.architecture, var.virttype, var.storagetype)}' found")}"
 }
-
